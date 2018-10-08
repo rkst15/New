b1ff26dea5c2f3ef80a5fbea43fd85c312e5655d
Ryohei Kita <rkstsoccer15@gmail.com>
2018-10-08 18:16:11 +0800

    new

---
+++ cal.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>cal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+
+<body>
+    <form action="" method="post">
+        <input type="number" name="first">
+        <select name="mark" id="">
+            <option value="+">+</option>
+            <option value="-">-</option>
+        </select>
+        <input type="number" name="second">
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</body>
+
+</html>
+<?php
+if (isset($_POST['submit'])) {
+    $first = $_POST['first'];
+    $second = $_POST['second'];
+    $mark = $_POST['mark'];
+    if ($mark == '+') {
+        $total = $first + $second;
+        echo $total;
+    } else {
+        $total = $first - $second;
+        echo $total;
+    }
+}
+?>
